daf1b77becb00d102b1cdfec0ad42e8ab9e72bd0
Petar Španja <petar@spanja.info>
2018-08-29 10:32:36 +0200

    QueryType 3 cleanup

--- src/Netgen/Bundle/MoreDemoBundle/Controller/FullViewController.php
+++ src/Netgen/Bundle/MoreDemoBundle/Controller/FullViewController.php
@@ -54,46 +54,4 @@ class FullViewController extends Controller
 
         return $view;
     }
-
-    public function viewArticle(ContentView $view, Request $request)
-    {
-        $content = $view->getSiteContent();
-
-        if (!$content->getField('tags')->isEmpty()) {
-
-            $tags = $content->getFieldValue('tags');
-
-            $tagsIds = array_map(
-                function(Tag $tag) {
-                    return $tag->id;
-                },
-                $tags->tags
-            );
-
-            $criteria = [
-                new Criterion\Visibility(Criterion\Visibility::VISIBLE),
-                new TagId($tagsIds),
-            ];
-
-            $query = new LocationQuery();
-            $query->limit = 100;
-            $query->filter = new Criterion\LogicalAnd($criteria);
-            $query->sortClauses = [
-                new SortClause\DatePublished(LocationQuery::SORT_DESC),
-            ];
-
-            $pager = $this->getFindPager(
-                $query,
-                $request->query->get('page', 1),
-                3
-            );
-
-            $view->addParameters([
-                'pager' => $pager,
-            ]);
-        }
-
-
-        return $view;
-    }
 }
